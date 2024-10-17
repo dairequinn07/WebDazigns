6786793fe8758140b7c05aca1f93cf889c516596
Daire Quinn <dairequinn@hotmail.co.uk>
2024-10-17 10:15:46 +0100

    CSS style changes

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 
 # Step 3: Add HTTPS redirection before any request is processed
-# @app.before_request
-# def https_redirect():
-#     if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
-#         # Redirect HTTP requests to HTTPS
-#         return redirect(request.url.replace('http://', 'https://', 1), code=301)
+@app.before_request
+def https_redirect():
+    if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
+        # Redirect HTTP requests to HTTPS
+        return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
 
 @app.after_request
