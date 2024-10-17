c63ab983e5060d0d83f66b59a6a847b5f031e3d7
Daire Quinn <dairequinn@hotmail.co.uk>
2024-10-17 17:42:08 +0100

    CSS style changes

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 
 # Step 3: Add HTTPS redirection before any request is processed
-# @app.before_request
-# def https_redirect():
-#     if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
-#         # Redirect HTTP requests to HTTPS
-#         return redirect(request.url.replace('http://', 'https://', 1), code=301)
+@app.before_request
+def https_redirect():
+    if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
+        # Redirect HTTP requests to HTTPS
+        return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
 
 @app.after_request
